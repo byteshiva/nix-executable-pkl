5cccdc9e0d704bdfa49f352c76bb23972fb63d82
Siva <byteshiva@users.noreply.github.com>
2024-02-11 18:53:08 +0530

    Update file paths and variable references in flake.nix
    Changed 'jpklPath = ./jpkl;' to 'jpklPath = "./jpkl";' and modified conditional statements for better variable handling in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         jpklUrl = "https://repo1.maven.org/maven2/org/pkl-lang/pkl-cli-java/0.25.2/pkl-cli-java-0.25.2.jar";
-        jpklPath = ./jpkl;
+        jpklPath = "./jpkl";
       in pkgs.mkShell {
         buildInputs = [
           pkgs.jdk  # Add JDK as a dependency
@@ -20,9 +20,9 @@
 
         shellHook = ''
           # Download jpkl only if it doesn't exist
-          if [ ! -e $jpklPath ]; then
-            curl -L -o $jpklPath $jpklUrl
-            chmod +x $jpklPath
+          if [ ! -e ${jpklPath} ]; then
+            curl -L -o ${jpklPath} ${jpklUrl}
+            chmod +x ${jpklPath}
           fi
           
           export PATH="$PWD:$PATH"  # Add the current directory to the PATH
